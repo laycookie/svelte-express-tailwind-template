a6d349d7ba43643a7accfe8ba29dad2b190946d2
LAYTORANIME <lodennis6@gmail.com>
2021-11-23 19:25:55 -0800

    fix
    adds the rest

---
+++ frontend/src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<h1 class="text-red-800">Hello {name}!</h1>
+</main>
+
+<style lang="postcss" global>
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+</style>
